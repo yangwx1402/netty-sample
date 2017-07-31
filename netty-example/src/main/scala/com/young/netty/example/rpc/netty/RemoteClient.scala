b426ec1af503e5b66d7edbfb7cf7962e5d40f91f
yangyong <yangyong3@le.com>
2017-07-31 16:44:53 +0800

    netty http sample

--- netty-example/src/main/scala/com/young/netty/example/rpc/netty/RemoteClient.scala
+++ netty-example/src/main/scala/com/young/netty/example/rpc/netty/RemoteClient.scala
@@ -3,7 +3,7 @@ package com.young.netty.example.rpc.netty
 import java.io.IOException
 
 import io.netty.bootstrap.Bootstrap
-import io.netty.channel.ChannelInitializer
+import io.netty.channel.{ChannelFuture, ChannelInitializer}
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioSocketChannel
@@ -13,25 +13,26 @@ import io.netty.channel.socket.nio.NioSocketChannel
   */
 class RemoteClient(initHandler: ChannelInitializer[SocketChannel]) extends RemoteTrait {
 
-  def connect(ip: String = "localhost", port: Int = 8080): Unit = {
+  @throws[IOException]
+  def connect(ip: String = "localhost", port: Int = 8080, close: Boolean): ChannelFuture = {
     val bootStrap = new Bootstrap
     val pool = new NioEventLoopGroup(10)
     try {
       bootStrap.group(pool).channel(classOf[NioSocketChannel]).option(tcp_nodelay, true).handler(initHandler)
       val futureChannel = bootStrap.connect(ip, port).sync()
-      futureChannel.channel().closeFuture().sync()
+      if (close)
+        futureChannel.channel().closeFuture().sync()
+      futureChannel
     } catch {
       case e: IOException => println("断开连接")
-        System.exit(-1)
-    }finally{
-        pool.shutdownGracefully()
+        throw e;
     }
   }
 }
 
 object RemoteClient {
-  def connect(ip: String = "localhost", port: Int = 8080, initHandler: ChannelInitializer[SocketChannel]): Unit = {
+  def connect(ip: String = "localhost", port: Int = 8080, initHandler: ChannelInitializer[SocketChannel],close:Boolean = true): ChannelFuture = {
     val client = new RemoteClient(initHandler)
-    client.connect(ip, port)
+    client.connect(ip, port,close)
   }
 }
